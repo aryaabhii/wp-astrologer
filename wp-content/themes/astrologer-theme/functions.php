627d96bbcac43c9bd012f0306c4b07488f7d45dc
aryaabhii <aryaabhi84@gmail.com>
2026-09-03 00:41:24 +0530

    Added automatic CSS cache-busting versioning in functions.php

--- wp-content/themes/astrologer-theme/functions.php
+++ wp-content/themes/astrologer-theme/functions.php
@@ -51,8 +51,8 @@ function astroveda_scripts() {
 	// FontAwesome icons
 	wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css', array(), '6.5.1' );
 
-	// Theme Main Stylesheet
-	wp_enqueue_style( 'astroveda-style', get_stylesheet_uri(), array(), '1.0.0' );
+	// Theme Main Stylesheet (with automatic cache busting)
+	wp_enqueue_style( 'astroveda-style', get_stylesheet_uri(), array(), filemtime( get_stylesheet_directory() . '/style.css' ) );
 
 	// Main JavaScript
 	wp_enqueue_script( 'astroveda-js', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0.0', true );
